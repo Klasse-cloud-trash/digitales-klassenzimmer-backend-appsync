820eb972d94044ec243e5abecad0287408f0aa5b
David Kagerer <david@binder-kagerer.de>
2020-03-21 23:08:53 +0100

    🎉adds appsync and dynamodb

--- lib/digitales-klassenzimmer-backend-appsync-stack.ts
+++ lib/digitales-klassenzimmer-backend-appsync-stack.ts
@@ -1,9 +1,30 @@
 import * as cdk from '@aws-cdk/core';
+import { GraphQLApi } from '@aws-cdk/aws-appsync'
+import { Table, BillingMode, AttributeType } from '@aws-cdk/aws-dynamodb'
+
 
 export class DigitalesKlassenzimmerBackendAppsyncStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const appSync = new GraphQLApi(this, 'DigitalClassRoomApi', {
+      name: 'DigitalClassRoomApi',
+      schemaDefinitionFile: './schema.graphql',
+    });
+
+    const userTable = new Table(this, 'UserTable', {
+        billingMode: BillingMode.PAY_PER_REQUEST,
+        partitionKey: {
+            name: 'id',
+            type: AttributeType.STRING,
+        },
+    });
+
+    appSync.addDynamoDbDataSource(
+      'UserDataSource',
+      'The users data source',
+      userTable
+    );
+
   }
 }
